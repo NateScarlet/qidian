eb8470f321ef0b1544eb94836ce965949590d666
NateScarlet <NateScarlet@Gmail.com>
2023-04-26 20:09:12 +0800

    test: update expected url

--- pkg/author/author_test.go
+++ pkg/author/author_test.go
@@ -16,6 +16,6 @@ func TestFetch(t *testing.T) {
 	assert.Equal(t, "4362771", author.ID)
 	assert.Equal(t, "7687417", author.UserID)
 	assert.Equal(t, "忘语", author.Name)
-	assert.Equal(t, "https://facepic.qidian.com/qd_face/349573/a4362771/0", author.AvatarURL)
+	assert.Equal(t, "https://ccportrait.yuewen.com/apimg/349573/p_3626391704023601/100", author.AvatarURL)
 	assert.Equal(t, "阅文集团白金作家，《凡人修仙传》一书在业内创造传奇，成为“凡人流”作品开山鼻祖。", author.Biography)
 }
